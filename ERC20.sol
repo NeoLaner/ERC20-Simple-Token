82a85732ddefb2b9bfa8c301659700b79cad8b3a
NeoLaner <neo.laner.official@gmail.com>
2025-08-11 23:23:20 +0330

    init

---
+++ ERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+contract ERC20 {
+    string constant public  name = "MyTokenName";
+    string constant public  symbol = "MTN";
+    uint8 constant public  decimals = 18;
+}
