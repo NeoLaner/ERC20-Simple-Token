371090d5c32b2eaa68e7afcc4b6f1243d54ec770
NeoLaner <neo.laner.official@gmail.com>
2025-08-12 14:42:57 +0330

    add transfer method

--- ERC20.sol
+++ ERC20.sol
@@ -6,4 +6,16 @@ contract ERC20 {
     string constant public  name = "MyTokenName";
     string constant public  symbol = "MTN";
     uint8 constant public  decimals = 18;
+
+    uint public totalSupply; 
+    mapping (address account => uint) balanceOf;
+
+    function  transfer(address to  ,uint amount ) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount , "Insufficient Balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        return true;
+    }
+
 }
